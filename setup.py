eda80fdb3c6fd0a23bbce437713c8fc3b308fca1
Sjoerd Arendsen <sjoerd@optixdesigns.com>
2012-03-02 11:29:40 +0100

    Include package data

--- setup.py
+++ setup.py
@@ -4,14 +4,21 @@ from setuptools import setup, find_packages
 long_description = open('README.md').read()
  
 setup(
-    name='django-hub-flag',
-    version='0.1',
-    description='Flags for django',
-    long_description=long_description,
-    author='Sjoerd Arendsen',
-    author_email='s.arendsen@hub.nl',
-    url='https://github.com/hub-nl/nl.hub.django.app.flag/',
-    packages=['flag'],
-    include_package_data=True,
-    zip_safe=False,
+  name='django-hub-flag',
+  version='0.1',
+  description='Flags for django',
+  long_description=long_description,
+  author='Sjoerd Arendsen',
+  author_email='s.arendsen@hub.nl',
+  url='https://github.com/hub-nl/nl.hub.django.app.flag/',
+  packages=(
+    'flag',
+    'flag.templatetags',
+  ),
+  package_data={
+    'flag': [
+      'templates/flag/*',
+    ]
+  },
+  zip_safe=False,
 )
