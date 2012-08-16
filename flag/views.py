07233af1c98cda8056cff396dc88c1c2f23055d7
Sjoerd Arendsen <sjoerd@optixdesigns.com>
2012-08-16 17:41:07 +0300

    Update flag/views.py

--- flag/views.py
+++ flag/views.py
@@ -14,6 +14,7 @@ from django.forms.models import model_to_dict
 from django.template.loader import render_to_string
 from django.views.generic import ListView
 from django.views.decorators.cache import never_cache
+from flag.utils import generate_unflag_url
 import hashlib
 
 @login_required
@@ -69,6 +70,8 @@ def flag(request, ftype, ct, pk, token, action=None):
     data = {}
     data['success'] = str(success)
     data['state'] = str(state)
+    #data['unflag_url'] = generate_unflag_url(ftype, request.user, obj)
+    #data['flag_url'] = str(state)
     data['ftype'] = model_to_dict(ftype)
     return HttpResponse(simplejson.dumps(data), content_type="text/javascript")
   else:
